e76e2aba8cbe402a3b27155d388c282a5b893d73
TopAcademy2022 <popov_r_top@outlook.com>
2023-07-23 20:13:30 +0300

    Updated the project architecture. Closed #11

--- src/script.js
+++ src/script.js
@@ -1,12 +1,310 @@
+class Task
+{
+	// Task name
+	#_name;
+
+	// Task priority
+	#_priority;
+
+	// Task date
+	#_date;
+
+	// Task status
+	#_isDone;
+
+	constructor(taskName, taskPriority = Number(0), taskDate = new Date(), taskIsDone = Boolean(false))
+	{
+		this.#_name = String(taskName);
+		this.#_priority = Number(taskPriority);
+		this.#_date = new Date(taskDate);
+		this.#_isDone = Boolean(taskIsDone);
+	}
+
+	get name()
+	{
+		return this.#_name;
+	}
+
+	set name(newName)
+	{
+		this.#_name = String(newName);
+	}
+
+	get status()
+	{
+		return this.#_isDone;
+	}
+
+	set status(newStatus)
+	{
+		this.#_isDone = Boolean(newStatus);
+	}
+}
+
+class TaskList
+{
+	// List of tasks
+	#_taskList;
+
+	constructor()
+	{
+		this.#_taskList = new Array();
+	}
+
+	get taskList()
+	{
+		return this.#_taskList;
+	}
+
+	// Remove all tasks from task list
+	#RemoveAllTasks()
+	{
+		this.#_taskList.splice(0, this.#_taskList.length);
+	}
+
+	#ClearRenderContainer()
+	{
+		let tasksContainer = document.getElementById('container');
+		tasksContainer.innerHTML = null;
+	}
+
+	static FindIndexTaskInList(taskList, findedTask)
+	{
+		let indexOfTask = -1;
+		let iterator = 0;
+
+		for(let taskInList of taskList)
+		{
+			if(taskInList.name === findedTask.name &&
+				taskInList.status === findedTask.status)
+			{
+				indexOfTask = iterator;
+				break;
+			}
+			iterator++;
+		}
+
+		return indexOfTask;
+	}
+
+	#RenderTask(task, taskList)
+	{
+		// Render size
+		const SIZE_INDENT_LEFT = 3;
+		const SIZE_CENTER_DIV = 6;
+
+		// Create main row for task
+		let taskRowHtml = document.createElement('div');
+		taskRowHtml.classList.add('row');
+
+		// Create margin left for main row
+		let divMarginLeft = document.createElement('div');
+		divMarginLeft.classList.add(`col-${SIZE_INDENT_LEFT}`);
+
+		// Create main div with task contents
+		let divTaskContents = document.createElement('div');
+		divTaskContents.classList.add(`col-${SIZE_CENTER_DIV}`);
+
+		// Create input group for task text and control buttons
+		let taskContentsInputGroup = document.createElement('div');
+		taskContentsInputGroup.classList.add('input-group');
+		taskContentsInputGroup.classList.add('mb-3');
+		taskContentsInputGroup.classList.add('mt-3');
+
+		// Create input with task text
+		let taskAsInputField = document.createElement('input');
+		taskAsInputField.classList.add('form-control');
+		taskAsInputField.classList.add('text-center');
+
+		// Set task color
+		if (task.status)
+		{
+			taskAsInputField.classList.add('text-success');
+		}
+		else
+		{
+			taskAsInputField.classList.add('text-danger');
+		}
+
+		// Set task text
+		taskAsInputField.value = task.name;
+
+		// Create button for switch task status
+		let switchTaskStatusButton = document.createElement('button');
+		switchTaskStatusButton.classList.add('btn');
+		switchTaskStatusButton.classList.add('btn-primary');
+
+		// Create icon for button of switch task status
+		let iconSwitchTaskStatusButton = document.createElement('i');
+		iconSwitchTaskStatusButton.classList.add('bi');
+		iconSwitchTaskStatusButton.classList.add('bi-calendar-check');
+
+		// Add icon for button
+		switchTaskStatusButton.appendChild(iconSwitchTaskStatusButton);
+
+		// Create button for delete task
+		let deleteTaskButton = document.createElement('button');
+		deleteTaskButton.classList.add('btn');
+		deleteTaskButton.classList.add('btn-outline-danger');
+		deleteTaskButton.innerText = 'Delete';
+
+		// Visible elements for task contents in input group
+		taskContentsInputGroup.appendChild(taskAsInputField);
+		taskContentsInputGroup.appendChild(switchTaskStatusButton);
+		taskContentsInputGroup.appendChild(deleteTaskButton);
+
+		// Visible input group with task contents
+		divTaskContents.appendChild(taskContentsInputGroup);
+
+		// Visible main div with task contents
+		taskRowHtml.appendChild(divMarginLeft);
+		taskRowHtml.appendChild(divTaskContents);
+
+		// Get container for tasks
+		let tasksContainer = document.getElementById('container');
+		tasksContainer.appendChild(taskRowHtml);
+
+		// Add event for task input of set new task text
+		taskAsInputField.addEventListener('input', (event) => {
+			task.name = event.target.value;
+		});
+
+		// Add event for button of switch task status
+		switchTaskStatusButton.addEventListener('click', (event) => {
+			let targetElement = event.target;
+			let taskAsInputField;
+
+			if (targetElement.classList.contains('bi'))
+			{
+				taskAsInputField = targetElement.parentNode.parentNode.firstChild;
+			}
+			else
+			{
+				taskAsInputField = targetElement.parentNode.firstChild;
+			}
+
+			task.status = Boolean(!task.status);
+
+			if (task.status)
+			{
+				taskAsInputField.classList.remove('text-danger');
+				taskAsInputField.classList.add('text-success');
+			}
+			else
+			{
+				taskAsInputField.classList.remove('text-success');
+				taskAsInputField.classList.add('text-danger');
+			}
+		});
+
+		// Add event for button of delete task
+		deleteTaskButton.addEventListener('click', (event) => {
+			const DELETED_TASK_INDEX = TaskList.FindIndexTaskInList(taskList, task);
+			taskList = taskList.splice(DELETED_TASK_INDEX, 1);
+
+			let deletedTaskRow = event.target.parentNode.parentNode.parentNode;
+			let tasksContainer = document.getElementById('container');
+			tasksContainer.removeChild(deletedTaskRow);
+		});
+	}
+
+	// Add new task in task list
+	AddTask(task)
+	{
+		if (task instanceof Task)
+		{
+			this.#_taskList.push(task);
+		}
+		else
+		{
+			console.log('this object not Task member');
+		}
+	}
+
+	RenderAllTasks()
+	{
+		this.#ClearRenderContainer();
+
+		for(let task of this.#_taskList)
+		{
+			this.#RenderTask(task, this.#_taskList);
+		}
+	}
+
+	LoadTasksFromFile(eventOpenFile)
+	{
+		if (this.#_taskList.length)
+		{
+			if (confirm('Хотите ли удалить уже существующие задачи?'))
+			{
+				this.#RemoveAllTasks();
+			}
+		}
+
+		let fileReader = new FileReader();
+		fileReader.readAsText(eventOpenFile.files[0]);
+
+		let currentInstance = this;
+		fileReader.onloadend = function () {
+			let dataFromFile = JSON.parse(fileReader.result);
+			
+			for (let taskFromFile of dataFromFile)
+			{
+				let newTask = new Task(taskFromFile.taskName, taskFromFile.taskStatus);
+				currentInstance.AddTask(newTask);
+				currentInstance.RenderAllTasks();
+			}
+		};
+	}
+
+	SaveTasksToFile(downloadButton, fileName)
+	{
+		const FILE_TYPE = '.json';
+
+		let taskList = new Array();
+		const keysForJsonFile = ['taskName', 'taskStatus'];
+
+		if(this.#_taskList.length)
+		{
+			let tasksAsJson = new Array();
+
+			for(let task of this.#_taskList)
+			{
+				let taskAsJson = {
+					[keysForJsonFile[0]]: task.name,
+					[keysForJsonFile[1]]: task.status
+				};
+				tasksAsJson.push(taskAsJson);
+			}
+	
+			var downloadFile = new Blob([JSON.stringify(tasksAsJson)], { type: 'text/plain' });
+	
+			downloadButton.href = URL.createObjectURL(downloadFile);
+			downloadButton.download = fileName + FILE_TYPE;
+		}
+		else
+		{
+			if (Number(String(downloadButton.href).indexOf('blob')) != -1)
+			{
+				downloadButton.href = '#';
+				downloadButton.download = null;
+			}
+			alert('Задач для сохранения не существует!');
+		}
+	}
+}
+
+// MAIN task list
+const TASK_LIST = new TaskList();
+
 let AddEventEnterKeyUp = () =>
 {
 	const ENTER_KEY_CODE = 13;
-	let textAddedTask = document.getElementById('task-text');
-	
-	textAddedTask.addEventListener('keyup', (event) => {
-		if(event.keyCode === ENTER_KEY_CODE)
-		{
-			AddTask(textAddedTask.value);
+	let taskText = document.getElementById('task-text');
+
+	taskText.addEventListener('keyup', (event) => {
+		if (event.keyCode === ENTER_KEY_CODE) {
+			AddTask();
 		}
 	});
 }
@@ -18,209 +316,52 @@ let AddEvents = () =>
 
 let GetTextFromTaskInputField = () =>
 {
-	let taskText = String(document.getElementById('task-text').value);
+	let taskText = document.getElementById('task-text').value;
 	return taskText;
 }
 
 let ClearTextFromTaskInputField = () =>
 {
-	let resultText = document.getElementById('task-text');
-	resultText.value = null;
+	let taskText = document.getElementById('task-text');
+	taskText.value = null;
 }
 
 let CheckStringAllCharsIsSpace = (str) =>
 {
 	let result = true;
-	
-	for(let charInString of String(str))
+
+	for (let charInString of String(str))
 	{
-		if(charInString != ' ')
+		if (charInString != ' ')
 		{
 			result = false;
 			break;
 		}
 	}
-	
+
 	return result;
 }
 
-let AddTask = (taskText, taskIsDone = false) =>
+let AddTask = () =>
 {
-	const INDENT_LEFT = 3;
-	const CENTER_DIV_SIZE = 6;
-	
-	if(String(taskText) && !CheckStringAllCharsIsSpace(taskText))
-	{
-		let newRow = document.createElement('div');
-		newRow.classList.add('row');
-		
-		let divMarginLeft = document.createElement('div');
-		divMarginLeft.classList.add(`col-${INDENT_LEFT}`);
-		
-		let divInputGroup = document.createElement('div');
-		divInputGroup.classList.add(`col-${CENTER_DIV_SIZE}`);
-		
-		let inputGroup = document.createElement('div');
-		inputGroup.classList.add('input-group');
-		inputGroup.classList.add('mb-3');
-		inputGroup.classList.add('mt-3');
-		
-		let inputTaskText = document.createElement('input');
-		inputTaskText.classList.add('form-control');
-		inputTaskText.classList.add('text-center');
-		
-		if(taskIsDone)
-		{
-			inputTaskText.classList.add('text-success');
-		}
-		else
-		{
-			inputTaskText.classList.add('text-danger');
-		}
-		
-		inputTaskText.value = String(taskText);
-		
-		let switchStatusButton = document.createElement('button');
-		switchStatusButton.classList.add('btn');
-		switchStatusButton.classList.add('btn-primary');
-		
-		let iconForSwitchStatusButton = document.createElement('i');
-		iconForSwitchStatusButton.classList.add('bi');
-		iconForSwitchStatusButton.classList.add('bi-calendar-check');
-		
-		switchStatusButton.appendChild(iconForSwitchStatusButton);
-		
-		switchStatusButton.addEventListener('click', (event) => {
-			let targetElement = event.target;
-			let taskTextAsHtml;
-			
-			if(targetElement.classList.contains('bi'))
-			{
-				taskTextAsHtml = targetElement.parentNode.parentNode.firstChild;
-			}
-			else
-			{
-				taskTextAsHtml = targetElement.parentNode.firstChild;
-			}
-			
-			if(taskTextAsHtml.classList.contains('text-success'))
-			{
-				taskTextAsHtml.classList.remove('text-success');
-				taskTextAsHtml.classList.add('text-danger');
-			}
-			else
-			{
-				taskTextAsHtml.classList.remove('text-danger');
-				taskTextAsHtml.classList.add('text-success');
-			}
-		});
-		
-		let deleteButton = document.createElement('button');
-		deleteButton.classList.add('btn');
-		deleteButton.classList.add('btn-outline-danger');
-		deleteButton.innerText = 'Delete';
-		deleteButton.addEventListener('click', (event) => {
-			let deleteRow = event.target.parentNode.parentNode.parentNode;
-			let rowContainer = document.getElementById('container');
-			rowContainer.removeChild(deleteRow);
-		});
-		
-		inputGroup.appendChild(inputTaskText);
-		inputGroup.appendChild(switchStatusButton);
-		inputGroup.appendChild(deleteButton);
-		
-		divInputGroup.appendChild(inputGroup);
-		
-		newRow.appendChild(divMarginLeft);
-		newRow.appendChild(divInputGroup);
+	let taskText = GetTextFromTaskInputField();
 
-		let adminElement = document.getElementById('container');
-		adminElement.appendChild(newRow);
-	
-		ClearTextFromTaskInputField();
-	}
-}
-
-let CheckAvailableTask = () =>
-{
-	let result = Boolean(false);
-	
-	let taskContainer = document.getElementById('container');
-	let taskListAsHtml = taskContainer.childNodes;
-	
-	if(taskListAsHtml.length)
+	if (String(taskText) && !CheckStringAllCharsIsSpace(taskText))
 	{
-		result = true;
+		let newTask = new Task(taskText);
+		TASK_LIST.AddTask(newTask);
+		TASK_LIST.RenderAllTasks();
 	}
-	
-	return result;
+
+	ClearTextFromTaskInputField();
 }
 
 let LoadDataFromFile = (eventOpenFile) =>
 {
-	if(!CheckAvailableTask())
-	{
-		let reader = new FileReader();
-		reader.readAsText(eventOpenFile.files[0]);
-	
-		reader.onload = function() {
-		let dataFromFile = JSON.parse(reader.result);
-		
-			for(let taskFromFile of dataFromFile)
-			{
-				AddTask(taskFromFile.taskName, taskFromFile.taskStatus);
-			}
-		};
-	}
-	else
-	{
-		if(confirm('Хотите ли удалить уже существующие задачи?'))
-		{
-			alert('Функционал в разработке..................');
-		}
-	}
+	TASK_LIST.LoadTasksFromFile(eventOpenFile);
 }
 
 let SaveDataToFile = (downloadButton, fileName = 'data') =>
 {
-	const FILE_TYPE = '.json';
-	
-	let taskList = [];
-	const keysForJsonFile = ['taskName', 'taskStatus'];
-	
-	let taskListAsHtml = document.getElementById('container').childNodes;
-	
-	if(CheckAvailableTask())
-	{
-		for(let taskAsHtml of taskListAsHtml)
-		{
-			let taskTextAsHtml = taskAsHtml.lastChild.firstChild.firstChild;
-			
-			let taskStatus = false;
-			if(taskTextAsHtml.classList.contains('text-success'))
-			{
-				taskStatus = true;
-			}
-			
-			let taskAsObject = {[keysForJsonFile[0]] : taskTextAsHtml.value,
-				[keysForJsonFile[1]] : taskStatus};
-			taskList.push(taskAsObject);
-		}
-	
-		const taskListAsJson = JSON.stringify(taskList);
-	
-		var file = new Blob([taskListAsJson], {type: 'text/plain'});
-	
-		downloadButton.href = URL.createObjectURL(file);
-		downloadButton.download = fileName + FILE_TYPE;
-	}
-	else
-	{
-		if(Number(String(downloadButton.href).indexOf('blob')) != -1)
-		{
-			downloadButton.href = '#';
-			downloadButton.download = null;
-		}
-		alert('Задач для сохранения не существует!');
-	}
+	TASK_LIST.SaveTasksToFile(downloadButton, fileName);
 }
