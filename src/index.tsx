455e62b067c7f75cced961c0f3e23c0079b9ea77
TopAcademy2022 <popov_r_top@outlook.com>
2024-05-29 20:47:48 +0300

    Update

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.css';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+	document.getElementById('container') as HTMLElement
 );
 
 root.render(
-  <React.StrictMode>
-  </React.StrictMode>
+	<React.StrictMode>
+	</React.StrictMode>
 );
